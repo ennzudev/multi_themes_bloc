bf00544fdc70fdc5ab571441231ca7f2568bc723
TMPO <nunezp20@gmail.com>
2023-03-24 08:45:46 -0600

    colors added

--- lib/theme/app_themes.dart
+++ lib/theme/app_themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:themed_app/theme/color_schemes.dart';
 
 enum AppTheme {
   GreenLight,
@@ -24,6 +25,28 @@ final appThemeData = {
       brightness: Brightness.dark,
     ),
   ),
-  // TODO: cree la variante blue light y blue dark
+  AppTheme.BlueLight: ThemeData(
+    primaryColor: Colors.lightBlue,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.lightBlue,
+      brightness: Brightness.light,
+    ),
+  ),
+  AppTheme.BlueDark: ThemeData(
+    primaryColor: Colors.blue[700],
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue[700]!,
+      brightness: Brightness.dark,
+    ),
+  ),
+  AppTheme.PurpleLight: ThemeData(
+    primaryColor: Colors.purple,
+    colorScheme: purpleLightColorScheme,
+  ),
+  AppTheme.PurpleDark: ThemeData(
+    primaryColor: Colors.purple[700],
+    colorScheme: purpleDarkColorScheme,
+  ),
+
   // TODO: cree la variante con material 3 y los color schemas
 };
